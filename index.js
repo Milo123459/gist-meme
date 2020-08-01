1b11fc862f316b6b0daa37358155a0138528e219
Milo123459 <milo5huntington@gmail.com>
2020-08-01 11:50:45 +0100

    new update

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const Box = new GistBox({
   id: process.env.GIST_ID,
   token: process.env.GH_TOKEN,
 });
-;(async() => {
-    const imageapi = require('imageapi.js');
-    const img = await imageapi(process.env.SUBREDDIT, true);
-    console.log(img);
-    await Box.update({
-        content: `👀 Here is your daily meme from r/${process.env.SUBREDDIT}!\n🔗 ${img}`
-    })
+(async () => {
+  const imageapi = require("imageapi.js");
+  const img = await imageapi(process.env.SUBREDDIT, true);
+  console.log(img);
+  await Box.update({
+    content: `👀 Here is your daily meme from r/${process.env.SUBREDDIT}!\n🔗 ${img}`,
+  });
 })();
